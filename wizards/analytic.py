c1ebeb2034a5df0328f8395ec4f2465abe5d411c
Viktoras Norkus <viktoras@bmx.lt>
2017-03-06 13:19:30 +0200

    analytic.py

--- wizards/analytic.py
+++ wizards/analytic.py
@@ -23,25 +23,7 @@ class AnalyticLineInvoiceWizard(models.TransientModel):
 
     @api.multi
     def create_lines(self):
-        import pdb; pdb.set_trace()
         account_lines = self.env['account.analytic.line']
         account_invoice = self.env['account.invoice']
         account_invoice_lines = self.env['account.invoice.line']
-
-        if is_invoiced is False:
-            lines = self._context.get('active_ids')
-            for active in lines:
-                create_invoice = {
-                    'account_lines.name':
-                        account_invoice_lines.name,
-                    'self.product_id':
-                        account_invoice_lines.product_id,
-                    'account_lines.project_id.partner_id':
-                        account_invoice.partner_id,
-                    'account_lines.unit_amount':
-                        account_invoice_lines.quantity,
-                }
-
-            new_invoice = account_invoice.create(create_invoice)
-            return new_invoice
-        # return line's
+        
