b9196c5c09b3af1b572b2148a16268771859dfa1
Viktoras Norkus <viktoras@bmx.lt>
2017-03-07 15:58:39 +0200

    if statemnt added

--- wizards/analytic.py
+++ wizards/analytic.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from datetime import date
+
 from odoo import models, fields, api
 
 
@@ -26,24 +28,28 @@ class AnalyticLineInvoiceWizard(models.TransientModel):
         account_invoice = self.env['account.invoice']
         for rec in self:
             for line in rec.line_ids:
-                #    if rec.merge_timesheets is True:
-
-                line_vals = {
-                    'name': line.name,
-                    'account_id': line.account_id.id,
-                    'product_id': rec.product_id.id,
-                    'partner_id': line.project_id.partner_id.id,
-                    'price_unit': line.product_id.price,
-                    'quantity': line.unit_amount,
-                }
-                invoice_vals = {
-                    'partner_id': line.project_id.partner_id.id,
-                    'invoice_line_ids': [(0, 0, line_vals)],
-                }
-                line.is_invoiced = True
-        account_invoice.create(invoice_vals)
+                if rec.merge_timesheets is True:
+                    pass
+                else:
+                    line_vals = {
+                        'name': line.name,
+                        'account_id': line.account_id.id,
+                        'product_id': rec.product_id.id,
+                        'partner_id': line.project_id.partner_id.id,
+                        'price_unit': rec.product_id.price,
+                        'quantity': line.unit_amount,
+                    }
+                    invoice_vals = {
+                        'partner_id': line.project_id.partner_id.id,
+                        'date_invoice': date.today(),
+                        'invoice_line_ids': [(0, 0, line_vals)],
+                    }
+                    line.is_invoiced = True
+                    account_invoice.create(invoice_vals)
 
         """If "Merge timesheet entries" is checked, all the account.analytic.line
         should be summed into one and only one invoice line should be created.
-        If during creation of the wizard the selected account.analytic.line lines contain different partners,
-        the multiple invoices should be created. After invoices are created, a list of created invoices should be displayed."""
+        If during creation of the wizard the selected account.analytic.line
+        lines contain different partners,
+        the multiple invoices should be created. After invoices are created,
+         a list of created invoices should be displayed."""
