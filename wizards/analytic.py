72270070c2abbd60831b4f4451a399898007789d
Viktoras Norkus <viktoras@bmx.lt>
2017-03-07 10:58:33 +0200

    product type = service

--- wizards/analytic.py
+++ wizards/analytic.py
@@ -23,18 +23,20 @@ class AnalyticLineInvoiceWizard(models.TransientModel):
 
     @api.multi
     def create_lines(self):
-        #import pdb; pdb.set_trace()
         account_invoice = self.env['account.invoice']
         for rec in self:
             for line in rec.line_ids:
 
                 line_vals = {
                     'name': line.name,
-                    'product_id': rec.product_id,
-                    'account_invoice.partner_id': line.project_id.partner_id,
+                    'account_id': line.account_id.id,
+                    'product_id': rec.product_id.id,
+                    'partner_id': line.project_id.partner_id.id,
+                    'price_unit': line.product_id.price,
                     'quantity': line.unit_amount,
                 }
                 invoice_vals = {
-                    'invoice_line_ids': (0, _, line_vals),
+                    'partner_id': line.project_id.partner_id.id,
+                    'invoice_line_ids': [(0, 0, line_vals)],
                 }
         account_invoice.create(invoice_vals)
