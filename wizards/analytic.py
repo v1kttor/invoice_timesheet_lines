feda9d269b9200918c529fca5b1ba81cac055649
Viktoras Norkus <viktoras@bmx.lt>
2017-03-08 12:06:44 +0200

    update

--- wizards/analytic.py
+++ wizards/analytic.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
-
+import itertools
 from datetime import date
 
 from odoo import models, fields, api
+
+
+def by_line_partner(line):
+    return line.project_id.partner_id.id
 
 
 class AnalyticLineInvoiceWizard(models.TransientModel):
@@ -25,27 +29,36 @@ class AnalyticLineInvoiceWizard(models.TransientModel):
 
     @api.multi
     def create_lines(self):
-        account_invoice = self.env['account.invoice']
-        for rec in self:
-            for line in rec.line_ids:
-                if rec.merge_timesheets is True:
-                    pass
-                else:
-                    line_vals = {
-                        'name': line.name,
-                        'account_id': line.account_id.id,
-                        'product_id': rec.product_id.id,
-                        'partner_id': line.project_id.partner_id.id,
-                        'price_unit': rec.product_id.price,
-                        'quantity': line.unit_amount,
-                    }
-                    invoice_vals = {
-                        'partner_id': line.project_id.partner_id.id,
-                        'date_invoice': date.today(),
-                        'invoice_line_ids': [(0, 0, line_vals)],
-                    }
-                    line.is_invoiced = True
-                    account_invoice.create(invoice_vals)
+        account_invoice_obj = self.env['account.invoice']
+        for record in self:
+            grouped_lines = itertools.groupby(
+                record.line_ids.sorted(by_line_partner), key=by_line_partner)
+            for partner_id, lines in grouped_lines:
+
+                for line in lines:
+
+                    import pdb; pdb.set_trace()
+                    if record.merge_timesheets is True:
+                        pass
+                    else:
+                        invoice_line_vals = {
+                            'name': line.name,
+                            # 'account_id': line.account_id.id,
+                            'product_id': record.product_id.id,
+                            'price_unit': record.product_id.price,
+                            'quantity': line.unit_amount,
+                        }
+                        line.is_invoiced = True
+                        # invoice_line_list = [record.line_ids]
+                        invoice_line_list = [invoice_line_vals]
+                invoice_vals = {
+                    'partner_id': line.project_id.partner_id.id,
+                    'date_invoice': date.today(),
+                    'type': 'out_invoice',
+                    'invoice_line_ids': [(0, 0, invoice_line_list)],
+                    # 'invoice_line_ids': [(0, 0, invoice_line_vals)],
+                }
+                account_invoice_obj.create(invoice_vals)
 
         """If "Merge timesheet entries" is checked, all the account.analytic.line
         should be summed into one and only one invoice line should be created.
