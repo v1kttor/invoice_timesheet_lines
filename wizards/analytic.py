c55850b84e4ca518a50273b2787ba405f6a6e099
Viktoras Norkus <viktoras@bmx.lt>
2017-03-10 13:53:59 +0200

    compute_taxes() , ValidationError, UserError, is_invoiced=True and so on

--- wizards/analytic.py
+++ wizards/analytic.py
@@ -3,6 +3,7 @@ import itertools
 from datetime import date
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError, UserError
 
 
 def by_line_partner(line):
@@ -36,6 +37,8 @@ class AnalyticLineInvoiceWizard(models.TransientModel):
         }
         tmp_line = ivl_obj.new(line_vals)
         tmp_line._onchange_product_id()
+        if not tmp_line.account_id:
+            raise UserError("Please set Account for selected lines")
         tmpvals = [(f, tmp_line[f]) for f in tmp_line._fields]
         line_vals.update({
             # Jei merginam eilutes, tai 'name' tures ateiti is tmp_line.name
@@ -46,37 +49,43 @@ class AnalyticLineInvoiceWizard(models.TransientModel):
             'invoice_line_tax_ids': [
                 (6, 0, tmp_line.invoice_line_tax_ids.ids)
             ],
+            'price_subtotal': tmp_line.price_subtotal
         })
         return line_vals
 
     def _prepare_invoice_vals(self, partner, lines):
-        vals = {
-            'partner_id': partner.id,
-            'date_invoice': date.today(),
-            'type': 'out_invoice',
-        }
-        return vals
+        if not partner.id:
+            raise UserError("Please set Customer for selected lines")
+        else:
+            vals = {
+                'partner_id': partner.id,
+                'date_invoice': date.today(),
+                'type': 'out_invoice',
+            }
+            return vals
 
     @api.multi
     def create_lines(self):
-        # Reikia patikrinti ar projektai turi priskirta partner_id (Customer)
-        # ant sukurto invoice pakviesti compute_taxes ir paziureti ar taxai yr
         invoice_obj = self.env['account.invoice']
         line_obj = self.env['account.invoice.line']
         for record in self:
             grouped_lines = itertools.groupby(
-                record.line_ids.sorted(by_line_partner), key=by_line_partner)
+                record.line_ids.sorted(
+                    by_line_partner), key=by_line_partner)
             for partner, lines in grouped_lines:
                 invoice_vals = self._prepare_invoice_vals(partner, lines)
                 invoice = invoice_obj.create(invoice_vals)
-
                 for line in lines:
-                    inv_line = line_obj.create(self._prepare_invoice_line_vals(
-                        invoice, record.product_id, line
-                    ))
+                    if line.is_invoiced:
+                        raise ValidationError("Line is already invoiced")
+                    inv_line = line_obj.create(
+                        self._prepare_invoice_line_vals(
+                            invoice, record.product_id, line
+                            ))
+                    invoice.compute_taxes()
+                    line.is_invoiced = True
                     # reikia susieti line (account.analytic.line) su sukurta
                     # invoice line (inv_line) ir is_invoiced uzsettinti i True
-
     """If "Merge timesheet entries" is checked, all the account.analytic.line
     should be summed into one and only one invoice line should be created.
     If during creation of the wizard the selected account.analytic.line
