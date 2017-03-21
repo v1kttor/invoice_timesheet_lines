039d6ba336187e4a96fb3e5c713564c277bdbebd
Viktoras Norkus <viktoras@bmx.lt>
2017-03-21 11:57:53 +0200

    tests in a single module

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import test_lines, test_no_customer, test_already_invoiced
-from . import test_invoice_line_different_customers, test_merge_lines
-from . import test_merge_and_two_different_customers
+from . import test_invoice_wizard
